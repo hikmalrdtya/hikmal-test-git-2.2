3b6caf4cc94cb68f4719892128df61040e48109d
Hikmal99 <hikmalraditya299@gmail.com>
2024-02-28 21:36:01 +0700

    menambahkan style pada index.php

--- index.php
+++ index.php
@@ -4,6 +4,12 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Coba Git Remote</title>
+    <style>
+        body{
+            margin: 0;
+            padding: 0;
+        }
+    </style>
 </head>
 <body>
     <h1>Hello World!!!</h1>
